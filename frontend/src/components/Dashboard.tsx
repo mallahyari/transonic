e2bb7dbaefa29ac66ab3034478377c75704c19e4
Mehdi Allahyari <mehdi.allahyari@gmail.com>
2023-11-16 09:31:25 -0500

    frontend updated

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import AudioComponent from './AudioComponent';
 import { Flex, Typography } from 'antd';
+import MainLayout from './MainLayout';
 
 const { Title, Paragraph } = Typography;
 
 export default function Dashboard() {
-  return (
-    <div className="App">
-      <Flex vertical>
-        <div style={{ margin: 'auto', textAlign: 'center' }}>
-          <Title> Convert your thoughts into Tweets Fast!</Title>
-          <Paragraph style={{ fontSize: '18px' }}>
-            Start recording your thoughts. Please note that you can only record
-            30 seconds every time.
-          </Paragraph>
-        </div>
-        <AudioComponent />
-      </Flex>
-    </div>
-  );
+	return (
+		<MainLayout>
+			<div className="App">
+				<Flex vertical>
+					<div style={{ margin: 'auto', textAlign: 'center' }}>
+						<Title> Convert your thoughts into Tweets Fast!</Title>
+						<Paragraph style={{ fontSize: '18px' }}>
+							Start recording your thoughts. Please note that you can only record 30 seconds every
+							time.
+						</Paragraph>
+					</div>
+					<AudioComponent />
+				</Flex>
+			</div>
+		</MainLayout>
+	);
 }
