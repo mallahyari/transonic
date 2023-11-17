cbeae9f2cfa03c88d31f2eab2c3e3fc2e1bc748f
mehdi <mehdi.allahyari@gmail.com>
2023-11-17 16:39:14 -0500

    vercel analytics added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import { Analytics } from '@vercel/analytics/react';
 import { Flex, Typography, ConfigProvider } from 'antd';
 import {
   ClerkProvider,
@@ -41,6 +41,7 @@ function App() {
         <SignedOut>
           <RedirectToSignIn />
         </SignedOut>
+        <Analytics />
       </ConfigProvider>
     </ClerkProvider>
   );
