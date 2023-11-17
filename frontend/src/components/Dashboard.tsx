2ce64de6fe17f81c59065ac2036c34130a307261
mehdi <mehdi.allahyari@gmail.com>
2023-11-17 16:30:34 -0500

    hero updated

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -6,20 +6,19 @@ import MainLayout from './MainLayout';
 const { Title, Paragraph } = Typography;
 
 export default function Dashboard() {
-	return (
-		<MainLayout>
-			<div className="App">
-				<Flex vertical>
-					<div style={{ margin: 'auto', textAlign: 'center' }}>
-						<Title> Convert your thoughts into Tweets Fast!</Title>
-						<Paragraph style={{ fontSize: '18px' }}>
-							Start recording your thoughts. Please note that you can only record 30 seconds every
-							time.
-						</Paragraph>
-					</div>
-					<AudioComponent />
-				</Flex>
-			</div>
-		</MainLayout>
-	);
+  return (
+    <MainLayout>
+      <div className="App">
+        <Flex vertical>
+          <div style={{ margin: 'auto', textAlign: 'center' }}>
+            <Title> Turn Your Thoughts into Tweets Quickly!</Title>
+            <Paragraph style={{ fontSize: '18px' }}>
+              Record up to 30 seconds at a time. Turn into a tweet!
+            </Paragraph>
+          </div>
+          <AudioComponent />
+        </Flex>
+      </div>
+    </MainLayout>
+  );
 }
